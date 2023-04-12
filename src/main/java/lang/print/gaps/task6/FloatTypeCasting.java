3002617ce26385952d89dc0a28cb2cdb153eeed2
vlshutyak <vl.shutyak@gmail.com>
2023-04-12 19:32:01 +0300

    s0-m3-t6

--- src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
+++ src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
@@ -2,5 +2,11 @@ package lang.print.gaps.task6;
 
 public class FloatTypeCasting {
     public void roundNumber(float numberToBeRounded) {
+        int rounded=Math.round( numberToBeRounded);
+        System.out.println(rounded);
+    }
+
+    public static void main(String[] args) {
+        new FloatTypeCasting().roundNumber(3.7f);
     }
 }
